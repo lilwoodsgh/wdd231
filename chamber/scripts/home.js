34fd5f33f757d88adf0d55e1729d3ff8e45f829f
lilwoodsgh <patamia16@gmail.com>
2025-04-16 08:46:08 +0000

    home responsive

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Weather info
     const weatherBox = document.getElementById("weather-info");
-  
     if (weatherBox) {
       weatherBox.innerHTML = `
         <p>🌤️ 29°C, Partly Cloudy</p>
         <p>Humidity: 65%</p>
       `;
-    } else {
-      console.error("Weather container not found!");
     }
+  
+    // Hamburger menu
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+  
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("show");
+    });
   });
   
